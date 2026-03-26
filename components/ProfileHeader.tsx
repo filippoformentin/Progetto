64328623f7bbae1e1d283b7dfabbec3d5656e692
filippoformentin <filippo.formentin@gmail.com>
2026-03-26 23:15:55 +0100

    Create ProfileHeader component

---
+++ components/ProfileHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfileHeader = () => {
+    return (
+        <div className="profile-header">
+            <h1>Profile Header</h1>
+            {/* Add additional profile information here */}
+        </div>
+    );
+};
+
+export default ProfileHeader;
